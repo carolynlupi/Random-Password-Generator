92464be4e44a0a637953aaa548766fec8ec432c0
Sage Lupi <Sage@Sages-MacBook-Pro.local>
2023-04-20 14:31:13 -0400

    Added Arrays for password

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,32 @@
 // Assignment code here
+var characterLength =8;
+var choiceArr = [];
+
+var specialCharArr = ['!','@','#','$','%','&','*','?','/','+','=',];
+var lowerCassArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+var upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+var numberArr = ['1','2','3','4','5','6','7','8','9','0'];
+
+function getPrompts() {
+  characterLength = parse(Intprompt("How many characters do you want your password to be? (8 - 128 characters"));
+
+}
+  
+
+
+
 function generatePassword () {
   console.log("Hi! You clicked the button!");
 
+///1. Prompt the user for the password Criteria
+//     A. password length 8 < 128
+//     B. lowercase, uppercase, special characters
+/// 2. Need to validate the input.
+/// 3. Display the generated password onto page.
 
 
 
 
-
-
-  
   return "Generated Password will go here!";
 }
 
