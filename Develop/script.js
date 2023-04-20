14da8a00d3cbdb32a979aca1fbc9c4f4baad2093
Sage Lupi <Sage@Sages-MacBook-Pro.local>
2023-04-20 15:37:56 -0400

    Added in alert if character length is not long enough or too long

--- Develop/script.js
+++ Develop/script.js
@@ -7,14 +7,6 @@ var lowerCassArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
 var upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
 var numberArr = ['1','2','3','4','5','6','7','8','9','0'];
 
-function getPrompts() {
-  characterLength = parse(Intprompt("How many characters do you want your password to be? (8 - 128 characters"));
-
-}
-  
-
-
-
 function generatePassword () {
   console.log("Hi! You clicked the button!");
 
@@ -22,6 +14,17 @@ function generatePassword () {
 //     A. password length 8 < 128
 //     B. lowercase, uppercase, special characters
 /// 2. Need to validate the input.
+
+function getPrompts() {
+  characterLength = parse(Intprompt("How many characters do you want your password to be? (8 - 128 characters"));
+
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert ("Character length must be between 8 - 128 characters. Please try again.");
+    return false;
+  }
+}
+  
+
 /// 3. Display the generated password onto page.
 
 
